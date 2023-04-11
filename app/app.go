f25e3f57a887ea3e918cfcfcbe32bbee46fc32d6
Lucas Gabriel <lucasgsilva2102@gmail.com>
2023-04-11 13:03:22 -0300

    feat: add command to find ips

--- app/app.go
+++ app/app.go
@@ -1,6 +1,12 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // generate cli app
 func Generate() *cli.App {
@@ -8,5 +14,31 @@ func Generate() *cli.App {
 	app.Name = "Command line interface"
 	app.Usage = "Find Ip's and server names"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Find Ip's",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name: "host",
+				},
+			},
+			Action: findIpsAndPrint,
+		},
+	}
+
 	return app
+}
+
+func findIpsAndPrint(context *cli.Context) {
+	host := context.String("host")
+
+	ips, err := net.LookupIP(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
